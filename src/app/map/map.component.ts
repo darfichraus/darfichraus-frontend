28470cfc96ab31f4d5373146d634a29222ef745d
Ron Kun <ronsen.1@gmx.de>
2020-03-22 18:21:35 +0100

    map offset
    map set view offset for sidebar

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -37,7 +37,7 @@ export class MapComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
     // ------ MAP + LAYER -------
-    this.coronamap = L.map('map', {zoomControl: false}).setView([51.27264, 9.26469], 6);
+    this.coronamap = L.map('map', {zoomControl: false}).setView([51.27264, 14.26469], 6);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
       id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, maxZoom: 8, minZoom: 6
