a9aab0e1cfeeff3eb1c722fa0115f98b40118268
Ron Kun <ronsen.1@gmx.de>
2020-03-24 16:14:06 +0100

    Imprint content
    Imporint content

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-impressum',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpressumComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ImpressumComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onClose(): void {
+    this.dialogRef.close();
   }
 
 }
