d7e7f39b360437f462a94bb47c0c5973c0b14e0d
Stefan Michalk <stefan.michalk@ecube.de>
2020-03-28 19:41:07 +0100

    prod-environment saved to api.darfichraus.de

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.crimsy.tech/',
+  apiUrl: 'https://api.darfichraus.de/',
   apiKey: '6bce1751a010f90b68eb887cae8e2141cce149d5de664412e67cdf150006aa16f2bd0373ce496aa5'
 };
